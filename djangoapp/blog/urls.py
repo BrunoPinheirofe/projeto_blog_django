215ed9c63638f14c80a1b37905c9c3f4cbba81cb
BrunoPinheirofe <bruno2013pinheiro@gmail.com>
2024-11-27 11:56:21 -0300

    utilizando context processor nos templates

--- djangoapp/blog/urls.py
+++ djangoapp/blog/urls.py
@@ -3,5 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('post/', views.post, name='index'),
+    path('page/', views.page, name='index'),
 ]
 
--- djangoapp/site_setup/context_processor.py
+++ djangoapp/site_setup/context_processor.py
@@ -3,7 +3,5 @@ from .models import SiteSetup
 def site_setup(request):
     data = SiteSetup.objects.order_by('-id').first()
     return{
-        'site_setup': {
-            'title': data
-        }
+        'site_setup': data
     }
